62768d18fc021a68f775fdb3a3feeb4b0d7bb5ba
Merlin Szymanski <merlin.szymanski@gmail.com>
2020-04-24 16:01:32 +0200

    adding gitignore

--- twio_web/settings.py
+++ twio_web/settings.py
@@ -105,13 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Berlin'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
