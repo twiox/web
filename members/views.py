c91a8765c2c33b7364749f6f5b0c0c7b53c2be3e
Merlin Szymanski <merlin.szymanski@gmail.com>
2020-04-29 01:34:53 +0200

    fixed minor issue

--- members/views.py
+++ members/views.py
@@ -38,7 +38,7 @@ class EventDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     
     def get_context_data(self, **kwargs):
         context = super(EventDetailView, self).get_context_data(**kwargs)
-        context['is_trainer'] = hasattr(request.user,"trainer")
+        context['is_trainer'] = hasattr(self.request.user,"trainer")
         return context
     
     def test_func(self):
