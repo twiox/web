d6545e8d0d5ba83924d25ae6df22d055c432e64d
Merlin Szymanski <merlin.szymanski@gmail.com>
2020-05-12 02:08:44 +0200

    the urls again... somehow lost them in the process

--- twio_web/urls.py
+++ twio_web/urls.py
@@ -31,10 +31,10 @@ urlpatterns = [
     path('remove_trainer/', user_views.remove_trainer, name="remove_trainer"),
     path("login/", auth_views.LoginView.as_view(template_name="user_handling/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="user_handling/logout.html"), name="logout"),
-    path("activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/", user_views.activate, name="activate"),
+    path("activate/<str:uidb64>/<str:token>/", user_views.activate, name="activate"),
     path("reset_pw/", auth_views.PasswordResetView.as_view(template_name="user_handling/password_reset.html"), name="password_reset"),
     path("reset_pw_done/", auth_views.PasswordResetDoneView.as_view(template_name="user_handling/password_reset_done.html"), name="password_reset_done"),
-    path(r"reset_pw/confirm/(<uidb64>[0-9A-Za-z]+)-<token>/$", auth_views.PasswordResetConfirmView.as_view(template_name="user_handling/password_reset_confirm.html"), name="password_reset_confirm"),
+    path("reset_pw/confirm/<str:uidb64>/<str:token>/", auth_views.PasswordResetConfirmView.as_view(template_name="user_handling/password_reset_confirm.html"), name="password_reset_confirm"),
     path("reset_pw/complete/",auth_views.PasswordResetCompleteView.as_view(template_name="user_handling/password_reset_complete.html"), name="password_reset_complete"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
