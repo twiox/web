7cb129c35c437611243d525988a68e7197198b3c
Merlin Szymanski <merlin.szymanski@gmail.com>
2020-09-24 12:50:17 +0200

    Put Database in separate folder for Docker volume

--- twio_web/settings.py
+++ twio_web/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'twio_web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'Database/db.sqlite3'),
     }
 }
 CONN_MAX_AGE = 300
