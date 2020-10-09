f10cbd8c6cf75242876d508c071d4e905ad834d5
Merlin Szymanski <merlin.szymanski@gmail.com>
2020-10-09 18:35:58 +0200

    Make URLs german, add 404-page

--- interested/urls.py
+++ interested/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path("", views.interested_index, name="interested_index"),
-    path("offers", views.interested_offers, name="offers"),
-    path("philosophy", views.interested_philosophy, name="philosophy"),
-    path("team", views.interested_team, name="team"),
-    path("team/new/leipzig/", views.TeamerLeipzigCreateView.as_view(),name="teamer_leipzig_create"),
-    path("team/new/jena/", views.TeamerJenaCreateView.as_view(),name="teamer_jena_create"),
-    path("team/<int:pk>/update/", views.TeamerUpdateView.as_view(),name="teamer_update"),
-    path("team/<int:pk>/delete/", views.TeamerDeleteView.as_view(),name="teamer_delete"),
+    path("interessierte-angebot", views.interested_offers, name="offers"),
+    path("interessierte-philosophie", views.interested_philosophy, name="philosophy"),
+    path("interessierte-team", views.interested_team, name="team"),
+    path("team/neu/leipzig/", views.TeamerLeipzigCreateView.as_view(),name="teamer_leipzig_create"),
+    path("team/neu/jena/", views.TeamerJenaCreateView.as_view(),name="teamer_jena_create"),
+    path("team/<int:pk>/ändern/", views.TeamerUpdateView.as_view(),name="teamer_update"),
+    path("team/<int:pk>/löschen/", views.TeamerDeleteView.as_view(),name="teamer_delete"),
 ]
 
