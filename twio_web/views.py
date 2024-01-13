5e479dd6b4ba89dceb5e85398d65d1d7842ded26
Merlin Szymanski <merlin_szymanski@eva.mpg.de>
2024-01-13 15:17:20 +0100

    Start reimplementation of page with materials-web light

--- twio_web/views.py
+++ twio_web/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 from interested.models import PublicEvent
 
+
 def landingPage(request):
     public_events = [x for x in PublicEvent.objects.all() if not x.is_past_due]
-    return render(request, "members/main/landing.html", context={'public_events':public_events})
+    return render(
+        request, "main/landing.html", context={"public_events": public_events}
+    )
+
 
 def impressumPage(request):
-    return render(request, "members/main/impressum.html", context={})
+    return render(request, "main/impressum.html", context={})
+
 
 def dataProtectionPage(request):
-    return render(request, "members/main/dataprotection.html", context={})
+    return render(request, "main/dataprotection.html", context={})
